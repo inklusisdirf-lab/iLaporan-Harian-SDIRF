c155c151fe3ce99861d85fcf383e67e65bc81ea6
Ardhy <ardhy.ridwanbadar@gmail.com>
2026-08-11 15:03:35 +0700

    fix: install missing dependency @ducanh2912/next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
+import type { NextConfig } from "next";
+
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
 });
 
-module.exports = withPWA({
-  // konfigurasi Anda
-});
+const nextConfig: NextConfig = {
+  // konfigurasi next.js lainnya
+};
+
+export default withPWA(nextConfig);
