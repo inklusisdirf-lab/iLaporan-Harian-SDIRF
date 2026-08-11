383e59885a58b41e36b1e726346f81be7cde4d33
Ardhy <ardhy.ridwanbadar@gmail.com>
2026-08-11 12:59:03 +0700

    feat: tambahkan persistSession true pada supabase client untuk menjaga sesi login tetap aktif

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,13 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: true, // Memastikan sesi disimpan di perangkat
-    autoRefreshToken: true, // Otomatis memperbarui token agar tidak kedaluwarsa
-    detectSessionInUrl: true, // Mendeteksi token dari URL (penting untuk login/reset password)
-    storage: typeof window !== 'undefined' ? window.localStorage : undefined, // Menyimpan ke LocalStorage HP
-  },
-})
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true, // Pastikan true agar sesi disimpan
+      autoRefreshToken: true, // Otomatis memperbarui token jika expired
+      detectSessionInUrl: true,
+    },
+  }
+);
