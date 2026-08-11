c0bb4e59163244759e32f459d7c2a860d65db32d
Ardhy <ardhy.ridwanbadar@gmail.com>
2026-08-11 13:07:49 +0700

    fix: gunakan createClient dari supabase-js untuk hindari error module missing

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,12 +1,12 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createBrowserClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
     auth: {
-      persistSession: true, // Pastikan true agar sesi disimpan
-      autoRefreshToken: true, // Otomatis memperbarui token jika expired
+      persistSession: true,
+      autoRefreshToken: true,
       detectSessionInUrl: true,
     },
   }
